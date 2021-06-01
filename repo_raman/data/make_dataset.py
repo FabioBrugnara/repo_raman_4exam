efdd30440dfa9d4907757ade0318d2a6165f8f08
Fabio Brugnara <fabio.brugnara-1@studenti.unitn.it>
2021-06-01 22:32:59 +0200

    initial commit from template

---
+++ repo_raman/data/make_dataset.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import click
+import logging
+from pathlib import Path
+
+
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
+
+def preprocess(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+
+    preprocess() # Aggiungere i nomi dei files!
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Il progetto riguarda il riconoscimento di materiali tramite gli spettri Raman.',
+    author='F e Andrea Pegoretti',
+    license='MIT',
+)
