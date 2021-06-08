81a8410c9305eb97e7151459c19e0ab3ea111d2f
Fabio Brugnara <fabio.brugnara-1@studenti.unitn.it>
2021-06-08 11:59:43 +0200

    Iiziato il fit

--- notebooks/module.py
+++ notebooks/module.py
@@ -6,7 +6,6 @@ def raman_plot():
     """
     Funzione che serve per settare la dimensione dell'immgine e i label degli assi per un plot di spettro Raman.
     """
-    import matplotlib.pyplot as plt
     plt.figure(figsize=(16,6))
     plt.xlabel("wave number [1/cm]")
     plt.ylabel("Intensity")
